eae469791fffec8e3b06c7e4cc3f7cd3bf5e1d54
Carlos Montoya <dikarey@gmail.com>
2019-08-09 10:52:04 -0500

    App ProductsController

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Data;
+
+namespace Shop.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(productRepository.GetAll());
+        }
+    }
+}
