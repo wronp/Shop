cd85a4e269e9e09281c016ab7d9c1a266bff686c
Carlos Montoya <dikarey@gmail.com>
2019-08-12 12:05:05 -0500

    NewUserViewModel

--- Shop.Web/Controllers/AccountController.cs
+++ Shop.Web/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shop.Web.Controllers
 {
+    using Data.Entities;
     using Helper;
     using Models;
 
@@ -49,13 +51,64 @@ namespace Shop.Web.Controllers
 
         public async Task<IActionResult> Logout()
         {
-            await this.userHelper.LogoutAsync();
-            return this.RedirectToAction("Index", "Home");
+            await userHelper.LogoutAsync();
+            return RedirectToAction("Index", "Home");
         }
-
-
         #endregion
 
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterNewUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userHelper.GetUserByEmailAsync(model.Username);
+                if (user == null)
+                {
+                    user = new User
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Username,
+                        UserName = model.Username
+                    };
+
+                    var result = await userHelper.AddUserAsync(user, model.Password);
+                    if (result != IdentityResult.Success)
+                    {
+                        ModelState.AddModelError(string.Empty, "The user couldn't be created.");
+                        return View(model);
+                    }
+
+                    #region Automatic login if the user is already created.
+                    var loginViewModel = new LoginViewModel
+                    {
+                        Password = model.Password,
+                        RememberMe = false,
+                        UserName = model.Username
+                    };
+
+                    var result2 = await userHelper.LoginAsync(loginViewModel);
+
+                    if (result2.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    ModelState.AddModelError(string.Empty, "The user couldn't be login.");
+                    return View(model);
+                    #endregion
+                }
+
+                ModelState.AddModelError(string.Empty, "The username is already registered.");
+            }
+
+            return View(model);
+        }
     }
 }
